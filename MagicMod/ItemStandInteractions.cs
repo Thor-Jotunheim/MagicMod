7993f8bdbcd9fa9de933e324d9b33501afb83fdf
Thor <thorfromjotunheim@gmail.com>
2025-01-21 10:42:03 -0600

    save

--- MagicMod/ItemStandInteractions.cs
+++ MagicMod/ItemStandInteractions.cs
@@ -1,5 +1,6 @@
 using HarmonyLib;
 using UnityEngine;
+using System.Collections;
 
 namespace MagicMod
 {
@@ -15,48 +16,55 @@ namespace MagicMod
         [HarmonyPatch(typeof(ItemStand), "Interact")]
         public class ItemStandInteractPatch
         {
-            static bool Prefix(ItemStand __instance, Player player, ref bool __result)
+            static bool Prefix(ItemStand __instance, Humanoid user, bool hold, bool alt, ref bool __result)
             {
-                // If the item is being placed
-                if (__instance.IsPlacingItem(player)) 
+                // If the player is holding the item and trying to place it
+                if (!hold && !__instance.HaveAttachment() && __instance.m_supportedItems.Count == 1)
                 {
-                    __instance.StartCoroutine(PlaceItemCoroutine(__instance, player.GetPlacedItem()));
+                    ItemDrop.ItemData item = user.GetInventory().GetItem(__instance.m_supportedItems[0].m_itemData.m_shared.m_name);
+                    if (item != null)
+                    {
+                        // Call the coroutine for placing the item (this will delay the placement for 3.8 seconds)
+                        __instance.StartCoroutine(PlaceItemCoroutine(__instance, item, user));
+                        __result = false;  // Prevent the default placement logic while waiting
+                        return false;  // Skip the original method
+                    }
+                    user.Message(MessageHud.MessageType.Center, "$piece_itemstand_missingitem");
                     __result = false;
                     return false;
                 }
-                return true;
-            }
-        }
 
-        // Harmony patch for Item Stand pickup (picking up an item takes 7.5 seconds)
-        [HarmonyPatch(typeof(ItemStand), "Interact")]
-        public class ItemStandPickupPatch
-        {
-            static bool Prefix(ItemStand __instance, Player player, ref bool __result)
-            {
-                if (__instance.IsPickingUpItem(player))
+                // If the player is picking up the item
+                if (__instance.HaveAttachment() && __instance.m_canBeRemoved)
                 {
-                    __instance.StartCoroutine(PickupItemCoroutine(__instance));
-                    __result = false;
-                    return false;
+                    // Call the coroutine for picking up the item (this will delay the pickup for 7.5 seconds)
+                    __instance.StartCoroutine(PickupItemCoroutine(__instance, user));
+                    __result = false;  // Prevent the default pickup logic while waiting
+                    return false;  // Skip the original method
                 }
+
+                // Allow the default interaction logic if no item is being placed or picked up
                 return true;
             }
         }
 
         // Coroutine for placing item (takes 3.8 seconds)
-        private static IEnumerator PlaceItemCoroutine(ItemStand itemStand, ItemDrop item)
+        public static IEnumerator PlaceItemCoroutine(ItemStand itemStand, ItemDrop.ItemData item, Humanoid user)
         {
-            yield return new WaitForSeconds(3.8f);
-            itemStand.PlaceItem(item);
+            yield return new WaitForSeconds(3.8f);  // Wait for 3.8 seconds before placing the item
+
+            // Perform the placement action using the interact method with the correct Humanoid
+            itemStand.Interact(user, false, false);  // Pass the player’s humanoid to the interact method
             Debug.Log("[MagicMod] Item placed on stand.");
         }
 
         // Coroutine for picking up item (takes 7.5 seconds)
-        private static IEnumerator PickupItemCoroutine(ItemStand itemStand)
+        public static IEnumerator PickupItemCoroutine(ItemStand itemStand, Humanoid user)
         {
-            yield return new WaitForSeconds(7.5f);
-            itemStand.PickUpItem();
+            yield return new WaitForSeconds(7.5f);  // Wait for 7.5 seconds before picking the item
+
+            // Perform the pickup action using the interact method with the correct Humanoid
+            itemStand.Interact(user, false, true);  // Pass the player’s humanoid to the interact method
             Debug.Log("[MagicMod] Item picked up from stand.");
         }
     }
